49b5e031bb3a93f59ea1fdc674acc4f96b0c77ab
apin <apin.misc@gmail.com>
2026-01-14 23:52:05 +0200

    cosmetics

--- src/main/java/com/apintea/store/store_management_api/repositories/ProductRepository.java
+++ src/main/java/com/apintea/store/store_management_api/repositories/ProductRepository.java
@@ -3,7 +3,6 @@ package com.apintea.store.store_management_api.repositories;
 import com.apintea.store.store_management_api.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, UUID> {
